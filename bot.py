6dd57a6661ba3ecbe0d9f6bf70017699cc2ee3c5
sourovyt <94685518+sourovyt@users.noreply.github.com>
2026-06-16 05:20:22 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,12 @@ from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
 
-# handlers
 from handlers.start import router as start_router
 from handlers.playbook import router as playbook_router
 
 
 dp = Dispatcher()
 
-# register routers
 dp.include_router(start_router)
 dp.include_router(playbook_router)
 
@@ -19,7 +17,7 @@ dp.include_router(playbook_router)
 async def main():
     bot = Bot(token=BOT_TOKEN)
 
-    print("🦾 BitClaw AI Bot is running...")
+    print("BitClaw AI Running...")
 
     await dp.start_polling(bot)
 
