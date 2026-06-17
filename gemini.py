e12c73fb81083bcec0aecdea9ccea73222a10aba
sourovyt <94685518+sourovyt@users.noreply.github.com>
2026-06-17 09:28:23 +0600

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -1,4 +1,6 @@
 from google import genai
+from PIL import Image
+
 from config import GEMINI_API_KEY
 
 client = genai.Client(api_key=GEMINI_API_KEY)
@@ -9,6 +11,47 @@ async def generate_text(prompt):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
+    )
+
+    return response.text
+
+
+async def analyze_chart(image_path):
+
+    image = Image.open(image_path)
+
+    prompt = """
+You are a professional crypto technical analyst.
+
+Analyze this trading chart screenshot.
+
+Return ONLY in this format:
+
+📈 Trend
+• point
+
+🎯 Support
+• point
+
+🚧 Resistance
+• point
+
+⚠ Risks
+• point
+
+🚀 Trade Setup
+• point
+
+Rules:
+- Keep under 1200 characters
+- Use bullet points
+- No long paragraphs
+- Give realistic support and resistance levels visible on chart
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[prompt, image]
     )
 
     return response.text
